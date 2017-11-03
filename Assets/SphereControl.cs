55f59f4e334a80f68e2f69e2fcd59e2e4b4f17ee
SANGHALEE <sangha206@naver.com>
2017-11-03 11:47:26 +0900

    Moving
    movemove

---
+++ Assets/SphereControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereControl : MonoBehaviour
+{
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        transform.Translate(1f * Time.deltaTime, 0f, 0f);
+    }
+}
